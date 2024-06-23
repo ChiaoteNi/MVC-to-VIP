7839a174f364d4d1e2e421c0f1c01e862da385a2
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-06-23 12:27:50 +0800

    feat: migrate test cases
    - @Test & @Suite
    - test UI related object

--- ITApp/ITAppTests/MeetupEventListViewControllerTests.swift
+++ ITApp/ITAppTests/MeetupEventListViewControllerTests.swift
@@ -7,10 +7,34 @@
 //
 
 import XCTest
+import Testing
 @testable import ITApp
 
-class MeetupEventListViewControllerTests: XCTestCase {
-    
+struct MeetupEventListVCTests {
+
+    private var sut: MeetupEventListViewController
+
+    @MainActor
+    init() throws {
+        sut = .init()
+    }
+//
+//    deinit {
+//        sut = nil
+//    }
+
+    @Test @MainActor
+    func viewDidLoadBehavior() {
+        let interactorSpy: MeetupEventListBusinessLogicSpy = .init()
+        sut.cp_resetInteractor(interactor: interactorSpy)
+        sut.viewDidLoad()
+
+        #expect(interactorSpy.isFetchMeetupEventsCalled, "MeetupEventList should fetch events when viewDidLoad.")
+    }
+}
+
+final class MeetupEventListViewControllerTests: XCTestCase {
+
     private var sut: MeetupEventListViewController!
     
     override func setUpWithError() throws {
@@ -20,7 +44,17 @@ class MeetupEventListViewControllerTests: XCTestCase {
     override func tearDownWithError() throws {
         sut = nil
     }
-    
+
+    @Test @MainActor
+    func viewDidLoadBehavior() {
+        sut = .init()
+        let interactorSpy: MeetupEventListBusinessLogicSpy = .init()
+        sut.cp_resetInteractor(interactor: interactorSpy)
+        sut.viewDidLoad()
+
+        #expect(interactorSpy.isFetchMeetupEventsCalled, "MeetupEventList should fetch events when viewDidLoad.")
+    }
+
     func testShouldFetchMeetupEventsWhenViewDidLoad() throws {
         let interactorSpy: MeetupEventListBusinessLogicSpy = .init()
         sut.cp_resetInteractor(interactor: interactorSpy)
@@ -54,48 +88,45 @@ class MeetupEventListViewControllerTests: XCTestCase {
     }
 }
 
-extension MeetupEventListViewControllerTests {
-    
-    private class MeetupEventListBusinessLogicSpy: MeetupEventListBusinessLogic {
-        var isFetchMeetupEventsCalled: Bool = false
-        
-        func fetchMeetupEvents(request: MeetupEventList.FetchEvents.Request) {
-            isFetchMeetupEventsCalled = true
-        }
-        
-        func tapFavorite(request: MeetupEventList.TapFavorite.Request) { }
-        func subscribeFavoriteUpdate(request: MeetupEventList.SubscribeFavoriteUpdate.Request) { }
-        func unsubscribeFavoriteUpdate(request: MeetupEventList.UnsubscribeFavoriteUpdate.Request) { }
+private class MeetupEventListBusinessLogicSpy: MeetupEventListBusinessLogic {
+    var isFetchMeetupEventsCalled: Bool = false
+
+    func fetchMeetupEvents(request: MeetupEventList.FetchEvents.Request) {
+        isFetchMeetupEventsCalled = true
     }
-    
-    private class TableViewSpy: UITableView {
-        var isReloadDataCalled: Bool = false
-        
-        override func reloadData() {
-            isReloadDataCalled = true
-        }
+
+    func tapFavorite(request: MeetupEventList.TapFavorite.Request) { }
+    func subscribeFavoriteUpdate(request: MeetupEventList.SubscribeFavoriteUpdate.Request) { }
+    func unsubscribeFavoriteUpdate(request: MeetupEventList.UnsubscribeFavoriteUpdate.Request) { }
+}
+
+private class TableViewSpy: UITableView {
+    var isReloadDataCalled: Bool = false
+
+    override func reloadData() {
+        isReloadDataCalled = true
     }
-    
-    private enum Seed {
-        enum Event {
-            
-            static let historyEvent: MeetupEventList.DisplayHistoryEvent = .init(
-                id: "1",
-                title: "Clean Swift Workshop",
-                dateText: "11月08日",
-                hostName: "Aaron & Willian",
-                coverImageURL: nil,
-                favoriteButtonColor: .black
-            )
-            
-            static let dummyEvent: MeetupEventList.DisplayHistoryEvent = .init(
-                id: "2",
-                title: "Dummy",
-                dateText: "Dummy",
-                hostName: "Dummy",
-                coverImageURL: nil,
-                favoriteButtonColor: .black
-            )
-        }
+}
+
+private enum Seed {
+    enum Event {
+
+        static let historyEvent: MeetupEventList.DisplayHistoryEvent = .init(
+            id: "1",
+            title: "Clean Swift Workshop",
+            dateText: "11月08日",
+            hostName: "Aaron & Willian",
+            coverImageURL: nil,
+            favoriteButtonColor: .black
+        )
+
+        static let dummyEvent: MeetupEventList.DisplayHistoryEvent = .init(
+            id: "2",
+            title: "Dummy",
+            dateText: "Dummy",
+            hostName: "Dummy",
+            coverImageURL: nil,
+            favoriteButtonColor: .black
+        )
     }
 }
